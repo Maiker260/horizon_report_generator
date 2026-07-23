ffe5af6886ba1a0bce8949f04a05cd9ce99defc9
Maiker <gutierrezmaiker@gmail.com>
2026-07-22 20:22:55 -0600

    Add Replication Status Section

--- src/__main__.py
+++ src/__main__.py
@@ -2,12 +2,12 @@ import sys
 from pathlib import Path
 from src.main import main
 
-# if len(sys.argv) < 2:
-#     print("No ZIP file was provided.")
-#     sys.exit(1)
+if len(sys.argv) < 2:
+    print("No ZIP file was provided.")
+    sys.exit(1)
 
-# zip_path = Path(sys.argv[1])
-# feature = sys.argv[2]
+zip_path = Path(sys.argv[1])
+feature = sys.argv[2]
 
 # Test
 
@@ -39,10 +39,10 @@ from src.main import main
 # zip_path = Path("tmp") / "FIX-AGENT.zip"
 # zip_path = Path("tmp") / "FIX-UAG.zip"
 
-zip_path = Path("tmp") / "ReplicIssue.zip"
+# zip_path = Path("tmp") / "ReplicIssue.zip"
 # zip_path = Path("tmp") / "GloRepli.zip"
 
-feature = "summary_report"
+# feature = "summary_report"
 # feature = "log_analysis"
 
 main(zip_path, feature)
